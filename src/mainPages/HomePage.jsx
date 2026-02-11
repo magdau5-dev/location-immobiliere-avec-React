9f6ead7ab0b36522ed13dd5726ce25f4725147d2
Magdau5 <magdau5nelysis@gmail.com>
2026-02-11 22:11:20 +0100

    Add : navBar fonctionnal

--- src/mainPages/HomePage.jsx
+++ src/mainPages/HomePage.jsx
@@ -3,7 +3,6 @@ import HomeSection from "../sections/HomeSection";
 const HomePage = () => {
     return (
         <>
-            <h1>Home Page</h1>
             <div>
                 <HomeSection />
             </div>
