6ccedacbd025a4860ea0d8bd70f1373db9aa3267
Magdau5 <magdau5nelysis@gmail.com>
2026-02-11 22:55:34 +0100

    Add : banner fonctionnal

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,10 +1,21 @@
-const Banner = () => {
+const Banner = ({
+    className = "",
+    backgroundUrl,
+    overlayColor = "rgba(0, 0, 0, 0.6)",
+}) => {
     return (
-        <>
-            <div className="banner">
-                <p>Chez vous, partout et ailleurs</p>
-            </div>
-        </>
+        <div
+            className={`banner-container ${className}`}
+            style={{ backgroundImage: `url(${backgroundUrl})` }}
+        >
+            <div
+                className="banner-overlay"
+                style={{ backgroundColor: overlayColor }}
+            ></div>
+
+            <p className="banner-text">Chez vous, partout et ailleurs</p>
+        </div>
     );
 };
+
 export default Banner;
