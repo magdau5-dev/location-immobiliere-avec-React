102ee99c5bd4c24d4e1fba6d406ace3128af3ae8
Hugo Larcher <larcherhugo.alexi@free.fr>
2026-02-12 00:00:27 +0100

    Add : json elements into homepage

--- src/sections/HomeSection.jsx
+++ src/sections/HomeSection.jsx
@@ -1,18 +1,21 @@
 import Banner from "../components/Banner";
 import Thumb from "../components/Thumb";
+import logement from "../json/logement.json";
 
 const HomeSection = () => {
     return (
         <>
             <div>
                 <Banner backgroundUrl="/src/assets/coastalCliff.png" />
+
                 <div className="thumb-container">
-                    <Thumb />
-                    <Thumb />
-                    <Thumb />
-                    <Thumb />
-                    <Thumb />
-                    <Thumb />
+                    {logement.map((item) => (
+                        <Thumb
+                            key={item.id}
+                            cover={item.cover}
+                            title={item.title}
+                        />
+                    ))}
                 </div>
             </div>
         </>
