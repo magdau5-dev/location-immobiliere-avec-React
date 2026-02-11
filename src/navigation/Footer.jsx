e0c6224101658277aa9c32062ac3fe911b88772f
Hugo Larcher <larcherhugo.alexi@free.fr>
2026-02-11 23:03:39 +0100

    Modif : footer image

--- src/navigation/Footer.jsx
+++ src/navigation/Footer.jsx
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import BoxImg from "../components/BoxImg";
 
 const Footer = () => {
+    const navigate = useNavigate();
     return (
         <>
             <div className="footer-container">
-                <BoxImg />
+                <BoxImg
+                    src="/src/assets/logoKasa2.png"
+                    alt="Logo Kasa"
+                    width="122px"
+                    height="39px"
+                    onClick={() => navigate("/")}
+                />
                 <p>© 2020 Kasa. All rights reserved</p>
             </div>
         </>
