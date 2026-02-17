e730fdbf70d08e54f28fca7cc0d084b2e75395a3
Magdau5 <magdau5nelysis@gmail.com>
2026-02-17 20:55:12 +0100

    Add : page 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,29 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './index.sass';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./index.sass";
 
-import HomePage from './mainPages/HomePage.jsx';
-import Gallery from './mainPages/Gallery.jsx';
-import About from './mainPages/About.jsx';
-import DefaultRoute from './layout/DefaultRoute.js';
-import NotFound from './mainPages/NotFound.jsx';
+import HomePage from "./mainPages/HomePage.jsx";
+import Gallery from "./mainPages/Gallery.jsx";
+import About from "./mainPages/About.jsx";
+import DefaultRoute from "./layout/DefaultRoute.js";
+import NotFound from "./mainPages/NotFound.jsx";
 
-createRoot(document.getElementById('root')).render(
-	<StrictMode>
-		<BrowserRouter>
-      <Routes>
-        {/* Route d'entrée */}
-        <Route element={<DefaultRoute />}>
-          
-          {/* Routes de navigation  */}
-          <Route path='/' element={<HomePage />}/>
-          <Route path='/gallery' element={<Gallery />}/>
-          <Route path='/about' element={<About />} />
-          
-        </Route>
-         {/* Route de redirection pour la page erreur 404 */}
-        <Route path="/*" element={<NotFound />} />
-			</Routes>
-		</BrowserRouter>
-	</StrictMode>
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                {/* Route d'entrée */}
+                <Route element={<DefaultRoute />}>
+                    {/* Routes de navigation  */}
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/gallery" element={<Gallery />} />
+                    <Route path="/about" element={<About />} />
+                </Route>
+
+                {/* Route de redirection pour la page erreur 404 */}
+                <Route path="/*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 );
--- src/mainPages/NotFound.jsx
+++ src/mainPages/NotFound.jsx
@@ -1,9 +1,26 @@
-const NotFound = () => {
-  return (
-    <div>
-      <p> ici not found</p>
-    </div>
-  )
-}
+import { useNavigate } from "react-router-dom";
+import NavBar from "../components/NavBar";
+import Footer from "../navigation/Footer";
 
-export default NotFound
+const NotFound = () => {
+    const navigate = useNavigate();
+    return (
+        <div className="notFound-page">
+            <NavBar />
+
+            <div className="notFound-content">
+                <h4 className="notFound-404">404</h4>
+                <p style={{ margin: "0px" }}>
+                    Oups! La page que vous demandez n'existe pas.
+                </p>
+                <a className="redirection-404" onClick={() => navigate("/")}>
+                    Retournez sur la page d'accueil
+                </a>
+            </div>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default NotFound;
