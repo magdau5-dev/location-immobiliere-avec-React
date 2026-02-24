19c40ea0bcfaedc5d7a9c7db58188cea7c6fc3fa
Magdau5 <magdau5nelysis@gmail.com>
2026-02-25 00:34:21 +0100

    Modif : 404 notfound for mobile

--- src/mainPages/NotFound.jsx
+++ src/mainPages/NotFound.jsx
@@ -5,17 +5,22 @@ import Footer from "../navigation/Footer";
 const NotFound = () => {
     const navigate = useNavigate();
     return (
-        <div className="notFound-page">
-            <NavBar />
+        <div>
+            <div className="notFound-page">
+                <NavBar />
 
-            <div className="notFound-content">
-                <h4 className="notFound-404">404</h4>
-                <p style={{ margin: "0px" }}>
-                    Oups! La page que vous demandez n'existe pas.
-                </p>
-                <a className="redirection-404" onClick={() => navigate("/")}>
-                    Retournez sur la page d'accueil
-                </a>
+                <div className="notFound-content">
+                    <h4 className="notFound-404">404</h4>
+                    <p style={{ margin: "0px" }}>
+                        Oups! La page que vous demandez n'existe pas.
+                    </p>
+                    <a
+                        className="redirection-404"
+                        onClick={() => navigate("/")}
+                    >
+                        Retournez sur la page d'accueil
+                    </a>
+                </div>
             </div>
 
             <Footer />
