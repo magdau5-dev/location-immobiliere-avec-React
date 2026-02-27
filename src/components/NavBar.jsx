52ba4be1baea1a4dd02536c75e975c3f8c848df2
Magdau5 <magdau5nelysis@gmail.com>
2026-02-27 11:25:30 +0100

    Modif : Navbar underline on target page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,27 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import BoxImg from "./BoxImg";
 
 const NavBar = () => {
-    const navigate = useNavigate();
-
     return (
-        <>
-            <div className="navBar-container">
-                <div>
+        <div className="navBar-container">
+            <div>
+                <NavLink to="/">
                     <BoxImg
                         src="/src/assets/logoKasa.png"
                         alt="Logo Kasa"
-                        onClick={() => navigate("/")}
                         className="navbar-logo"
                     />
-                </div>
-
-                <div className="navBar-links">
-                    <a onClick={() => navigate("/")}>Accueil</a>
-                    <a onClick={() => navigate("/about")}>A Propos</a>
-                </div>
+                </NavLink>
             </div>
-        </>
+
+            <div className="navBar-links">
+                <NavLink to="/" end>
+                    Accueil
+                </NavLink>
+                <NavLink to="/about">A Propos</NavLink>
+            </div>
+        </div>
     );
 };
+
 export default NavBar;
